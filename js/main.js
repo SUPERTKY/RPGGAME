cfe4491fe2f85da2a3733bd88ee04a4f62571e8b
SUPERTKY <purple_chips@outlook.jp>
2025-02-01 11:20:10 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,10 +4,16 @@ const config = {
     height: document.documentElement.clientHeight,
     scene: [HomeScene, GameScene],
     scale: {
-        mode: Phaser.Scale.RESIZE,
-        autoCenter: Phaser.Scale.CENTER_BOTH
+        mode: Phaser.Scale.RESIZE, // 画面サイズに合わせる
+        autoCenter: Phaser.Scale.CENTER_BOTH // 中央揃え
     }
 };
 
-
 const game = new Phaser.Game(config);
+
+// ウィンドウサイズ変更時にキャンバスを修正（右端のズレ対策）
+window.addEventListener("resize", () => {
+    let newWidth = document.documentElement.clientWidth;
+    let newHeight = document.documentElement.clientHeight;
+    game.scale.resize(newWidth + 1, newHeight + 1); // 1px 追加してズレ防止
+});
