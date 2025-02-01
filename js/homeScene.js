aad63bb5a000b6cf5ffb4dfb283f6eefa7338c8f
SUPERTKY <purple_chips@outlook.jp>
2025-02-01 11:54:10 +0900

    Update homeScene.js

--- js/homeScene.js
+++ js/homeScene.js
@@ -6,16 +6,15 @@ class HomeScene extends Phaser.Scene {
     preload() {
         this.load.image("background", "assets/background.png");
         this.load.image("startButton", "assets/startButton.png");
-        this.load.audio("bgm", "assets/bgm.mp3");
+        this.load.audio("bgm", "assets/bgm.mp3"); // BGMをロード
     }
 
     create() {
-        // ✅ `IntroScene` を完全に停止
-        this.scene.stop("IntroScene");
-
+        // 背景画像を設定
         let bg = this.add.image(0, 0, "background").setOrigin(0, 0);
         bg.setDisplaySize(this.scale.width, this.scale.height);
 
+        // スタートボタン
         let button = this.add.image(this.scale.width / 2, this.scale.height / 2, "startButton").setScale(0.5);
         button.setInteractive();
 
@@ -25,10 +24,12 @@ class HomeScene extends Phaser.Scene {
             this.bgm.play();
         }
 
+        // スタートボタンを押すとゲーム開始
         button.on("pointerdown", () => {
             this.scene.start("GameScene");
         });
 
+        // タイトルテキスト
         this.add.text(this.scale.width / 2 - 100, 100, "My Phaser Game", {
             fontSize: "40px",
             fill: "#ffffff"
