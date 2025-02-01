8a043da66aaef447c2636404e2bb77667a51af1b
SUPERTKY <purple_chips@outlook.jp>
2025-02-01 11:28:43 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
 const config = {
     type: Phaser.AUTO,
-    width: document.documentElement.clientWidth, // より正確な幅を取得
-    height: document.documentElement.clientHeight,
+    width: window.innerWidth,
+    height: window.innerHeight,
     scene: [HomeScene, GameScene],
     scale: {
-        mode: Phaser.Scale.RESIZE, // ウィンドウサイズ変更に対応
-        autoCenter: Phaser.Scale.CENTER_BOTH // 中央揃え
+        mode: Phaser.Scale.FIT, // 画面サイズに自動調整
+        autoCenter: Phaser.Scale.CENTER_BOTH // 中央に配置
     }
 };
 
 const game = new Phaser.Game(config);
 
-// ウィンドウリサイズ時に修正
+// ウィンドウサイズ変更時に修正
 window.addEventListener("resize", () => {
-    let newWidth = document.documentElement.clientWidth;
+      let newWidth = document.documentElement.clientWidth;
     let newHeight = document.documentElement.clientHeight;
     game.scale.resize(newWidth, newHeight);
 });
+
+
