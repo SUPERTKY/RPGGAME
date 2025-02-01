ab2e508288061f3231b9cd9c6c411d8b9bfd7057
SUPERTKY <purple_chips@outlook.jp>
2025-02-01 11:33:16 +0900

    Update homeScene.js

--- js/homeScene.js
+++ js/homeScene.js
@@ -6,13 +6,13 @@ class HomeScene extends Phaser.Scene {
     preload() {
         this.load.image("background", "assets/background.png");
         this.load.image("startButton", "assets/startButton.png");
+        this.load.audio("bgm", "assets/bgm.mp3"); // 🎵 音楽をロード
     }
 
     create() {
-        // 背景画像を全画面に設定
-let bg = this.add.image(0, 0, "background");
-    bg.setOrigin(0, 0); 
-    bg.setDisplaySize(this.scale.width + 10, this.scale.height); // 右端に余裕を持たせる
+        // 背景
+        let bg = this.add.image(0, 0, "background").setOrigin(0, 0);
+        bg.setDisplaySize(this.scale.width, this.scale.height);
 
         // スタートボタン
         let button = this.add.image(this.scale.width / 2, this.scale.height / 2, "startButton").setScale(0.5);
@@ -21,10 +21,15 @@ let bg = this.add.image(0, 0, "background");
             this.scene.start("GameScene");
         });
 
-        // タイトルテキスト
-        this.add.text(this.scale.width / 2 - 100, 100, "RPGGAME", {
+        // タイトル
+        this.add.text(this.scale.width / 2 - 100, 100, "My Phaser Game", {
             fontSize: "40px",
             fill: "#ffffff"
         });
+
+        // 🎵 BGM 再生（ループ付き）
+        this.bgm = this.sound.add("bgm", { loop: true, volume: 0.5 });
+        this.bgm.play();
     }
 }
+
