3ca827dd933bc11452a3c602c9f05a1b06506bfd
SUPERTKY <purple_chips@outlook.jp>
2025-02-01 11:10:26 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 const config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    scene: [HomeScene, GameScene]
+    width: window.innerWidth,
+    height: window.innerHeight,
+    scene: [HomeScene, GameScene],
+    scale: {
+        mode: Phaser.Scale.RESIZE,  // 画面サイズに合わせる
+        autoCenter: Phaser.Scale.CENTER_BOTH // 中央揃え
+    }
 };
+
 
 const game = new Phaser.Game(config);
