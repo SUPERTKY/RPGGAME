5a2bb2d0f4ea9d2fe23bc5ee92d1cf2dfa57bbb8
SUPERTKY <purple_chips@outlook.jp>
2025-02-01 22:00:01 +0900

    Update gameScene.js

--- js/gameScene.js
+++ js/gameScene.js
@@ -37,35 +37,8 @@ class GameScene extends Phaser.Scene {
             this.newBgm.play();
         }
 
-        this.createFramedInputBox();
-
         this.matchingButton.on("pointerdown", () => {
             console.log("マッチングボタン（画像）が押されました");
         });
-    }
-
-    createFramedInputBox() {
-        if (this.inputBox) {
-            this.inputBox.destroy();
-        }
-
-        const input = document.createElement("input");
-        input.type = "text";
-        input.placeholder = "暗証番号を入力";
-        input.style.fontSize = "20px";
-        input.style.width = "200px";
-        input.style.padding = "10px";
-        input.style.textAlign = "center";
-        input.style.border = "5px solid gold";
-        input.style.borderRadius = "10px";
-        input.style.background = "linear-gradient(to bottom, #fff8dc, #f4e1c6)";
-        input.style.boxShadow = "0px 4px 8px rgba(0, 0, 0, 0.3)";
-        input.style.position = "absolute";
-        input.style.zIndex = "100";
-        input.style.transform = "translate(-50%, -50%)";
-
-        this.inputBox = this.add.dom(this.scale.width / 2, 200, input)
-            .setOrigin(0.5, 0.5)
-            .setDepth(3);
     }
 }
