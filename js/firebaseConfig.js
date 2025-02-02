c7aa31fddfe1a37ead6dcb8321e28a24ef2cd0a6
SUPERTKY <purple_chips@outlook.jp>
2025-02-02 21:16:42 +0900

    Update firebaseConfig.js

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,3 +1,12 @@
+
+// Firebase が正しくロードされているか確認
+if (typeof firebase === "undefined") {
+    console.error("Firebase SDK がロードされていません！");
+} else {
+    console.log("Firebase SDK 読み込み成功！");
+}
+
+// Firebase 設定
 const firebaseConfig = {
     apiKey: "AIzaSyCwYEJvpjVIA6mynBX0FWp-RbeqmE4hCA0",
     authDomain: "rpggame-b7e7e.firebaseapp.com",
@@ -7,5 +16,7 @@ const firebaseConfig = {
     messagingSenderId: "54622239169",
     appId: "1:54622239169:web:6abbb760805e63fa215740"
   };// Firebase の初期化
-  firebase.initializeApp(firebaseConfig);
-  const db = firebase.database();
+
+// Firebase 初期化
+firebase.initializeApp(firebaseConfig);
+window.db = firebase.database(); // グローバルに `db` を定義
